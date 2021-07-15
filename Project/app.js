2dc17d0e8b0a2484fc0662af3454e87d6cc8fe76
Gurol Bilgin <gurolbilgin@gmail.com>
2021-07-15 16:59:32 -0600

    adds initial files to the project

---
+++ Project/app.js
@@ -0,0 +1,10 @@
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+
+
+let buttonCheck = document.getElementById("check") 
+
+addEventListener("click", confetti())
+
+
+
+// confetti();
